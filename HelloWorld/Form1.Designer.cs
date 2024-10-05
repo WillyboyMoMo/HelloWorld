8c24fa87c168ab5031d9c3975612aceaa6f8e774
DESKTOP-7EUHOD5\user <c159@gmail.com>
2024-10-05 15:08:07 +0800

    重新上傳

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCount = new System.Windows.Forms.Button();
+            this.labTime = new System.Windows.Forms.Label();
+            this.labCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(359, 89);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 0;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.Location = new System.Drawing.Point(386, 144);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(33, 12);
+            this.labTime.TabIndex = 1;
+            this.labTime.Text = "label1";
+            // 
+            // labCount
+            // 
+            this.labCount.AutoSize = true;
+            this.labCount.Location = new System.Drawing.Point(388, 185);
+            this.labCount.Name = "labCount";
+            this.labCount.Size = new System.Drawing.Size(33, 12);
+            this.labCount.TabIndex = 2;
+            this.labCount.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labCount);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.btnCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Label labCount;
+    }
+}
+
