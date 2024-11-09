0252078deffc23a951a057079b9d7064e576d20c
DESKTOP-7EUHOD5\user <c159@gmail.com>
2024-11-09 23:59:49 +0800

    Apply .gitignore rules and refresh tracked files

--- HelloWorld/Counter.cs
+++ HelloWorld/Counter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,35 +10,27 @@ namespace HelloWorld
 
         public async Task StartCountingAsync(Action<int> onCountUpdated, int maxCount = 10)
         {
-            // 如果已經有計數進行中，先取消
             CancelCounting();
 
-            // 為新計數任務創建 CancellationTokenSource
             cancellationTokenSource = new CancellationTokenSource();
             var token = cancellationTokenSource.Token;
 
-            // 開始計數，從0數到maxCount
             for (int i = 0; i <= maxCount; i++)
             {
                 if (token.IsCancellationRequested)
                 {
-                    return; 
+                    return;
                 }
 
-                // 更新當前計數
                 onCountUpdated?.Invoke(i);
 
-                await Task.Delay(500); // 每0.5秒更新一次數字
+                await Task.Delay(500);
             }
         }
 
-        // 取消當前的計數
         public void CancelCounting()
         {
-            if (cancellationTokenSource != null)
-            {
-                cancellationTokenSource.Cancel();
-            }
+            cancellationTokenSource?.Cancel();
         }
     }
 }
--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using HelloWorld;
+using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,27 +13,24 @@ namespace HelloWorld
         {
             InitializeComponent();
             counters = new Counter();
-            StartClockAsync(); // 啟動時鐘顯示功能
+            StartClockAsync();
         }
 
-        // 時鐘顯示的邏輯，更新時間每秒刷新一次
         private async void StartClockAsync()
         {
             while (true)
             {
                 labTime.Text = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-                await Task.Delay(1000); // 每秒更新一次時間
+                await Task.Delay(1000);
             }
         }
 
-        // 計數按鈕的邏輯，每次按下時重新開始計數
         private async void btnCount_Click(object sender, EventArgs e)
         {
-            await counter.StartCountingAsync((count) =>
+            await counters.StartCountingAsync((count) =>
             {
                 labCount.Text = count.ToString();
             });
         }
     }
 }
-
